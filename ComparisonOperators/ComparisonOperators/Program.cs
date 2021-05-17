8f224181919ad86b9cc31571c1638b58689cf210
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-05-17 09:23:01 -0400

    Add files via upload

---
+++ ComparisonOperators/ComparisonOperators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b));
+
+            Console.WriteLine("*********");
+
+            var d = 1;
+            var e = 2;
+            var f = 3;
+            Console.WriteLine(f > e && f > d);
+            Console.WriteLine(f > e && f == d);
+            Console.WriteLine(f > e || f == d);
+            Console.WriteLine(!(f > e || f == d));
+        }
+    }
+}
